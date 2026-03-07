18b47b688127331deb946c4fa02074473ab98a5d
Rachel Bastié <rachel.bastie@outlook.fr>
2026-03-07 09:25:41 +0100

    Pages projets

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,14 +9,15 @@ import { z } from 'astro/zod';
 
 // 4. Définir votre/vos collection(s)
 const projets = defineCollection({ 
-  loader: glob({pattern:"**/*.md", base:"./src/content/projets"}),
+  loader: glob({pattern:"**/*.{md,mdx}", base:"./src/content/projets"}),
   schema: ({ image }) => z.object({
       id: z.string(),
       slug: z.string(),
       titre: z.string(),
-      tags: z.array(z.enum(["Webdesign", "Direction artistique", "Branding", "Motion design", "Identité Visuelle", "Typographie", "Landing page", "Stratégie de marque"])),
+      tags: z.array(z.enum(["Webdesign", "HTML", "CSS", "JavaScript", "Direction artistique", "Branding", "Motion design", "Identité Visuelle", "Typographie", "Landing page", "Stratégie de marque"])),
       bgColor: z.string(),
       foreground: z.string(),
+      couleurFondBtn: z.string(),
       lieu: z.string(),
       date: z.string(),
       cover1: z.object ({src: image(), alt: z.string()}),
---
+++ src/ordre-projets.ts
@@ -0,0 +1,14 @@
+export const ordreProjets = [
+  //une liste pr définir l'ordre d'affichage des projets
+  "rapport-de-stage-larac",
+  "cap-cathare",
+  "specimen-cathare",
+  "specimen-vista",
+  "affiche-triathlon",
+  "magazine-todays-points",
+  "the-twilight-zone",
+  "le-velo-sentimental",
+  "pigments-cine-concerts",
+  "rapport-de-stage-pict",
+  "bois-de-nagoya",
+];
